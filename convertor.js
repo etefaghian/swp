4b46e3183144976ff88215cc7663c4d7d92499d4
etefaghian <etefaghian@yahoo.com>
2022-06-30 16:50:27 +0430

    👍add convertor to file

---
+++ convertor.js
@@ -0,0 +1,15 @@
+import { readFileSync, writeFileSync } from "fs";
+
+export const convertor = (service, mapping) => {
+  const file = String(readFileSync("./output/" + service + ".txt"));
+  let modifiedFile;
+
+  for (const key in mapping) {
+    if (Object.hasOwnProperty.call(mapping, key)) {
+      const element = mapping[key];
+      modifiedFile = file.replaceAll(new RegExp(key, "g"), element);
+    }
+  }
+
+  console.log(modifiedFile);
+};
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { convertor } from "./convertor.js";
 import { InstrumentedMMU, MMU } from "./service1.js";
 import { InstrumentedService3 } from "./service3.js";
 import { InstrumentedService4 } from "./service4.js";
@@ -5,3 +6,5 @@ import { InstrumentedService4 } from "./service4.js";
 InstrumentedMMU.requestMemory(23);
 InstrumentedService3.run();
 InstrumentedService4.run();
+
+convertor("service4", { STR: "1111111111111111111111111111111111111" });
